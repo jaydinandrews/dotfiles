38c7cd128c2a5737ed2c1f3e5a37a92151e6a8b0
Jaydin Andrews <jaydin.andrews@gmail.com>
2025-12-03 17:54:22 -0500

    add awesomewm

---
+++ .config/awesome/core/autostart.lua
@@ -0,0 +1,5 @@
+local awful = require("awful")
+awful.util.spawn_with_shell("lxpolkit &")
+awful.util.spawn_with_shell("$HOME/.fehbg &")
+awful.util.spawn_with_shell("picom --experimental-backends --config ~/.config/picom/picom.conf &")
+awful.util.spawn_with_shell("dunst --config ~/.config/dunst/dunstrc &")
